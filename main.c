8760bcaf691c6849e8a5d135c3023a8106afb722
PromptKhunanont <156408481+PromptKhunanont@users.noreply.github.com>
2024-02-23 08:17:53 +0000

    QueueQ

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ int main(int argc , char **argv) {
   NodePtr headPtr=NULL;
    NodePtr tailPtr=NULL;
    int i, x;
-   enqueue(&headPtr, &tailPtr, 5);
+ /* enqueue(&headPtr, &tailPtr, 5);
    enqueue(&headPtr, &tailPtr, 6);
    enqueue(&headPtr, &tailPtr, 7);
    
@@ -20,7 +20,7 @@ int main(int argc , char **argv) {
    printf("%d\n", x);
 
    x = dequeue(&headPtr, &tailPtr);
-   printf("%d\n", x);
+   printf("%d\n", x); */
 // 5 x 9 7 x 8 2 x
 
 
@@ -31,22 +31,23 @@ int main(int argc , char **argv) {
    q.size=0;
    
 
-   enqueue_struct(&q,7);
+   /*enqueue_struct(&q,7);
    printf("%d\n",dequeue_struct(&q));
    printf("%d\n",dequeue_struct(&q));
    enqueue_struct(&q,8);
-   printf("%d\n",dequeue_struct(&q));
+   printf("%d\n",dequeue_struct(&q));*/
    
 
- /*for(i=1;i<argc;i++){
+ for(i=1;i<argc;i++){
         if(strcmp(argv[i],"x")==0){
-            x=dequeue(&headPtr,&tailPtr);
+            x=dequeue_struct(&q);
+            if(x!= 0)
             printf("dequeing %d\n",x);
         }
         else {
-       enqueue_struct(&headPtr,&tailPtr, atoi(argv[i]));
+       enqueue_struct(&q, atoi(argv[i]));
            
         }
- }*/
+ }
   return 0;
 }
